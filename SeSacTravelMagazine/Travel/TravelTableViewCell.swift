81c6c8f65b9a6c9e78556ea5e2fb7b0d41901b55
hwiwls <80039139+hwiwls@users.noreply.github.com>
2024-01-09 02:13:37 +0900

    여행 매거진 화면 UI 구현

---
+++ SeSacTravelMagazine/Travel/TravelTableViewCell.swift
@@ -0,0 +1,29 @@
+//
+//  TravelTableViewCell.swift
+//  SeSacTravelMagazine
+//
+//  Created by hwijinjeong on 1/8/24.
+//
+
+import UIKit
+
+class TravelTableViewCell: UITableViewCell {
+    
+    
+    @IBOutlet weak var travelImageView: UIImageView!
+    @IBOutlet weak var titleLabel: UILabel!
+    @IBOutlet weak var subtitleLabel: UILabel!
+    @IBOutlet weak var dateLabel: UILabel!
+    
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
