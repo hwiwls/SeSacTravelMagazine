7945cba89829a2334f04da44e7e1a06d08679393
hwiwls <80039139+hwiwls@users.noreply.github.com>
2024-01-11 00:51:19 +0900

    프로토콜 프로퍼티 적용

--- SeSacTravelMagazine/Travel/TravelTableViewCell.swift
+++ SeSacTravelMagazine/Travel/TravelTableViewCell.swift
@@ -7,8 +7,13 @@
 
 import UIKit
 
+// 프로토콜 프로퍼티 적용
+protocol TableViewCellProtocol {
+    var identifier: String { get set }
+}
+
 class TravelTableViewCell: UITableViewCell {
-    
+    var identifier: String = "travelCell"
     
     @IBOutlet weak var travelImageView: UIImageView!
     @IBOutlet weak var titleLabel: UILabel!
